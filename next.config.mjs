6f50f0aa2ba7ec3f6d831f4fe93a64946ac95c18
Amritpal <sharysingh1144@gmail.com>
2026-02-28 14:46:49 +0000

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "apsk-portofolio.vercel.app" }],
+        permanent: true,
+        destination: "https://apsk-portfolio.vercel.app/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
